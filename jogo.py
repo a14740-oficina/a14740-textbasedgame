0cb76405497d9e3e358014923d886bb16e82264a
André Vides <a14740@oficina.pt>
2024-10-24 14:56:27 +0100

    Create jogo.py

---
+++ jogo.py
@@ -0,0 +1,7 @@
+print("Bem vindo ")
+
+start=int(input("Para começar o jogo prima a tecla 1: "))
+if start == 1:
+  print("És vitinha  um bilionário que gostava muito de viajar com o seu jato privado. Seu destino favorito? São Paulo uma cidade no Brasil, todos os anos você a visita e esse ano não é diferente. Porém este ano um ciclone tropical que dificultava muito as viagens tanto de barco como de avião atacava as águas do pacífico, apesar do seu amigo e piloto particular André o avisar  que talvez o melhor era não fazer essa viagem ele insistiu, e vocês lá foram  sobrevoando o pacífico. Quando estavam quase a chegar depararam-se com uma enorme tempestade que vos engoliu por completo,o jato perdeu o controle e acabou por cair numa ilha chamada Ilha de Queimada Grande que fica situada a noroeste de São Paulo. O único sobrevivente é você Vitinha")
+
+  
